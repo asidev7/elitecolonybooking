e3de4428e2810952a4ddbe9d2b14cfef556d56f4
asidev7 <78739624+asidev7@users.noreply.github.com>
2022-06-14 16:29:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ assest/footer.php
@@ -0,0 +1,7 @@
+<footer class="tm-black-bg">
+		<div class="container">
+			<div class="row">
+				<p class="tm-copyright-text">Copyright &copy;  ELITE COLONY</p>
+			</div>
+		</div>		
+	</footer>
---
+++ index.php
@@ -0,0 +1,188 @@
+<!-- Include Head -->
+
+<?php include "assest/head.php"; ?>
+
+<body class="tm-gray-bg">
+  
+ <!-- Header -->
+
+ <?php include "assest/header.php" ?>
+	
+		<!-- Banner -->
+	<section class="tm-banner">
+		<!-- Flexslider -->
+		<div class="flexslider flexslider-banner">
+		  <ul class="slides">
+		    <li>
+			    <div class="tm-banner-inner">
+					<h1 class="tm-banner-title">Rester, c'est exister:  <span class="tm-yellow-text">mais voyager,</span> c'est vivre.</h1>
+					<p class="tm-banner-subtitle">Avec ELITE COLONY</p>
+					<a href="booking.php" class="tm-banner-link">Inscrivez vous </a>	
+				</div>
+				<img src="img/banner-1.jpg" alt="Image" />	
+		    </li>
+		    <li>
+			    <div class="tm-banner-inner">
+					<h1 class="tm-banner-title">Lutte contre le racisme, <span class="tm-yellow-text">le régionalisme</span> ,les préjugés,</h1>
+					<p class="tm-banner-subtitle">Amour , respect ,égalité ,partage , fraternité, travail,umanitaires</p>
+					<a href="booking.php" class="tm-banner-link">INSCRIPTION</a>	
+				</div>
+		      <img src="img/banner-2.jpg" alt="Image" />
+		    </li>
+		    <li>
+			    <div class="tm-banner-inner">
+					<h1 class="tm-banner-title">Offre des formation  <span class="tm-yellow-text">artisanales,</span>Aide des humanitaire </h1>
+					<p class="tm-banner-subtitle">ELITE AFRICA</p>
+					<a href="booking.php" class="tm-banner-link">INSCRIPTION</a>	
+				</div>
+		      <img src="img/banner-3.jpg" alt="Image" />
+		    </li>
+		  </ul>
+		</div>	
+	</section>
+
+	<!-- gray bg -->	
+	<section class="container tm-home-section-1" id="more">
+		<div class="row">
+			<!-- slider -->
+			<div class="flexslider flexslider-about effect2">
+			  <ul class="slides">
+			    <li>
+			      <img src="img/about-1.jpg" alt="image" />
+			      <div class="flex-caption">
+			      	<h2 class="slider-title">Welcome To ELITE COLONY</h2>
+
+			      	<h3 class="slider-subtitle">L’équipe d’ ELITE africa accompagnera des jeunes belges et béninois et les préparer à une rencontre interculturelle.</h3>
+
+			      	<p class="slider-description">
+			      		ELITE Colony organise des colonie de vacances , rencontre interculturelle dans le but de rassembler toutes personnes quelque soit la couleur de peau , le pays , quelque l'handicape ou le défaut et l'âge 
+               <a href="contact.php" target="_parent">contact us</a>. <br><br>
+                  Nous Échangeons et rencontre interculturelle pour déconstruire les stéréotypes et les préjugés, le racisme.
+               </p>
+			      	<div class="slider-social">
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-pinterest"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-google-plus"></i></a>
+			      	</div>
+			      </div>			      
+			    </li>
+			    <li>
+			      <img src="img/about-1.jpg" alt="image" />
+			      <div class="flex-caption">
+			      	<h2 class="slider-title">Thank you for choosing us!</h2>
+			      	<h3 class="slider-subtitle">L’équipe d’ ELITE africa accompagnera des jeunes belges et béninois et les préparer à une rencontre interculturelle. </h3>
+			      	<p class="slider-description">Ce projet propose à ces jeunes d’opérer une coupure radicale avec leurs milieux et fonctionnements habituels et de vivre une expérience socio-culturelle au sein de la ville de lokossa.<br><br>
+                 </p><br>
+
+             <p>L’équipe propose aussi des moments d’immersion dans les familles, des animations dans nos villages partenaires, dons dans des orphelinats ainsi que des visites culturelles et touristiques dans le sud et le centre du Bénin
+			      	<div class="slider-social">
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-pinterest"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-google-plus"></i></a>
+			      	</div>
+			      </div>			      
+			    </li>
+			    <li>
+			      <img src="img/about-1.jpg" alt="image" />
+			      <div class="flex-caption">
+			      	<h2 class="slider-title">Notre missions</h2>
+			      	<h3 class="slider-subtitle">Notre mission est de sensibiliter organiser des rencontres pour montrer au monde entier que nous somme les même et que nous appartenont à un même monde et aussi contribué à aider les démunies.</h3>
+			      	<p class="slider-description">Nos domaine d'action  sont :
+              Lutte contre le racisme, le régionalisme ,les préjugés,
+              Offre des formation artisanales,Les aides humanitaire
+              Lutte contre le racisme, le régionalisme ,les préjugés, Offre des formation artisanales,les aides humanitaire.
+              NOS VALEUR :Amour , respect ,égalité ,partage , fraternité, travail,umanitaires?</p>
+			      	<div class="slider-social">
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-pinterest"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-google-plus"></i></a>
+			      	</div>
+			      </div>			      
+			    </li>
+			    <li>
+			      <img src="img/about-1.jpg" alt="image" />
+			      <div class="flex-caption">
+			      	<h2 class="slider-title">NOS ACTIVITES </h2>
+			      	<h3 class="slider-subtitle">En savoir plus...</h3>
+			      	<p class="slider-description">De  nombreuses activités sont mettre en place pour ces jeunes (danse béninoise, formation artisanale en pagne tissé, en sculpture, en métier de mode et vêtements africaine, ludothèque, tennis de table, cours en langue fon, initiation musicale).</p>
+			      	<div class="slider-social">
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-youtube"></i></a>
+			      		<a href="#" class="tm-social-icon"><i class="fa fa-google-plus"></i></a>
+			      	</div>
+			      </div>			      
+			    </li>
+			  </ul>
+			</div>
+		</div>
+	
+	</p>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</section>
+
+
+<br><br>
+
+<!--L'include de footer-->
+<?php include "assest/footer.php"; ?>
+
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      		<!-- jQuery -->
+  	<script type="text/javascript" src="js/moment.js"></script>							<!-- moment.js -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>					<!-- bootstrap js -->
+	<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>	<!-- bootstrap date time picker js, http://eonasdan.github.io/bootstrap-datetimepicker/ -->
+	<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<!--
+	<script src="js/froogaloop.js"></script>
+	<script src="js/jquery.fitvid.js"></script>
+-->
+   	<script type="text/javascript" src="js/templatemo-script.js"></script>      		<!-- Templatemo Script -->
+	<script>
+		// HTML document is loaded. DOM is ready.
+		$(function() {
+
+			$('#hotelCarTabs a').click(function (e) {
+			  e.preventDefault()
+			  $(this).tab('show')
+			})
+
+        	$('.date').datetimepicker({
+            	format: 'MM/DD/YYYY'
+            });
+            $('.date-time').datetimepicker();
+
+			// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+		  	$('a[href*=#]:not([href=#])').click(function() {
+			    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			      var target = $(this.hash);
+			      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			      if (target.length) {
+			        $('html,body').animate({
+			          scrollTop: target.offset().top
+			        }, 1000);
+			        return false;
+			      }
+			    }
+		  	});
+		});
+		
+		// Load Flexslider when everything is loaded.
+		$(window).load(function() {	  		
+			
+//	For images only
+		    $('.flexslider').flexslider({
+			    controlNav: false
+		    });
+
+
+	  	});
+	</script>
+ </body>
+ </html>
